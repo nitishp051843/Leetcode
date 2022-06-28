de1aae19d9899050c81c2aa4c395772a4d7634f5
nitishp051843 <nitishp051843@gmail.com>
2022-06-28 20:26:39 +0530

    Created Project and Checking Git compatibility

---
+++ SDE Sheet Challenge/Day 1/Set Matrix Zeroes.py
@@ -0,0 +1,25 @@
+# Solution 1 - Brute Force
+# Time : O(nm)
+# Space : O(n+m)
+
+class Solution:
+    def setZeroes(self, matrix: list[list[int]]) -> None:
+
+        m, n = len(matrix), len(matrix[0])
+
+        # Saving positions of 0
+        rows = []
+        cols = []
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 0:
+                    rows.append(i)
+                    cols.append(j)
+
+        # Checking each index position and giving the element index to 0
+        for i in range(m):
+            for j in range(n):
+                if i in rows or j in cols:
+                    matrix[i][j] = 0
+
+
